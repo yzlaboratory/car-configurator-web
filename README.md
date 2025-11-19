6ac9637c708457d4654e2e535b084c3638dd7f02
Yannik Zeyer <yannik.zeyer@fmc-ag.com>
2025-11-19 12:57:56 +0100

    added font and google symbols

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # car-configurator-web
 
 Angular Frontend calling 3 microservices with staging pipeline
+
+## Local Development
+
+Somehow ng serve does not trigger Tailwind JIT Compilation properly, use npm run start instead.
--- src/app/home/feature/configurator/configurator.ts
+++ src/app/home/feature/configurator/configurator.ts
@@ -2,6 +2,8 @@ import { ChangeDetectionStrategy, Component, inject, Signal } from '@angular/cor
 import { CatalogService } from '../../../shared/data-access/catalog.service';
 import { Catalog } from '../../../shared/data-access/entities/Catalog';
 import { toSignal } from '@angular/core/rxjs-interop';
+import { ConfigurationContainer } from '../../ui/configuration-container/configuration-container';
+import { Header } from '../../../shared/ui/header/header';
 
 @Component({
   selector: 'app-configurator',
@@ -9,6 +11,7 @@ import { toSignal } from '@angular/core/rxjs-interop';
   styleUrl: './configurator.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
+  imports: [ConfigurationContainer, Header],
 })
 export class Configurator {
   private service: CatalogService = inject(CatalogService);
