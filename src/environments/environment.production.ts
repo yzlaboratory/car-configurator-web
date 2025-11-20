67070dc387591bb8c6348b113e5953a3289b6522
Yannik Zeyer <yannik.zeyer@fmc-ag.com>
2025-11-20 14:58:45 +0100

    link changed

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  BASE_API_URL: `https://83dfjx7a2.execute-api.eu-central-1.amazonaws.com`,
+  BASE_API_URL: `https://8rqbc61nl6.execute-api.eu-central-1.amazonaws.com`,
   ASSET_API_URL: `https://e1hd7y382bspwz.cloudfront.net`,
 };
