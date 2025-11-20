2ed9cd0f71a1d2f389e1e9709476d107cb07f6ac
Yannik Zeyer <yannik.zeyer@fmc-ag.com>
2025-11-20 15:03:40 +0100

    linting fixed

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   BASE_API_URL: `https://8rqbc61nl6.execute-api.eu-central-1.amazonaws.com`,
-  ASSET_API_URL: `https://d1hd7y382bspwz.cloudfront.net',
+  ASSET_API_URL: `https://d1hd7y382bspwz.cloudfront.net`,
 };
